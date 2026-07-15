7542ee1b38e16a52c8cd6099abf56a332d6541b9
Aditya Patel <adityapatel124007@gmail.com>
2026-07-16 00:01:13 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.02%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,22 @@
+// class Solution {
+// public:
+//      int gcd(int a,int b){
+//         if(b==0){
+//             return a;
+//         }
+//        return gcd(b,a%b);
+//      }
+//     int gcdOfOddEvenSums(int n) {
+//         int sumofeven=(n*(n+1));
+//         int sumofodd=(n*n);
+//        return gcd(sumofeven,sumofodd);
+        
+//     }
+// };
+
+class Solution{
+    public:
+    int gcdOfOddEvenSums(int n){
+        return n;
+    }
+};
