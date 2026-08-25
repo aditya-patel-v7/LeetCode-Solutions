d1b13e3e91103ce65aa8d5302bb253b6a751d72b
Aditya Patel <adityapatel124007@gmail.com>
2026-08-26 01:11:12 +0530

    Time: 0 ms (100%), Space: 24 MB (82.02%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        
+        sort(nums.begin(),nums.end());
+        int ans=k;
+       
+        for(int i:nums){
+           if(i==ans){
+            ans+=k;
+         
+           }else{
+            continue;
+           }
+        }
+    return ans;}
+};
